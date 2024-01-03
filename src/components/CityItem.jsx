27d737bec8375669799f3ee9e538592bb5ff389e
John Doe <mmd.ayin.2020@gmail.com>
2024-01-03 21:17:10 +0330

    feat(CityItem): add active class to current city item

--- src/components/CityItem.jsx
+++ src/components/CityItem.jsx
@@ -3,6 +3,7 @@
 import { Link } from 'react-router-dom';
 import styles from './CityItem.module.css'
 import PropTypes from 'prop-types';
+import { useCities } from '../contexts/CitiesContext';
 
 const formatDate = (date) =>
     new Intl.DateTimeFormat("en", {
@@ -12,10 +13,13 @@ const formatDate = (date) =>
     }).format(new Date(date));
 
 function CityItem({ city }) {
+    const { currentCity } = useCities();
     const { cityName, emoji, date, id, position } = city;
     return (
         <li>
-            <Link className={styles.cityItem} to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
+            <Link
+                className={`${styles.cityItem} ${currentCity.id === id ? styles['cityItem--active'] : ''}`}
+                to={`${id}?lat=${position.lat}&lng=${position.lng}`}>
                 <span className={styles.emoji}>{emoji}</span>
                 <h3 className={styles.name}>{cityName}</h3>
                 <time className={styles.date}>{formatDate(date)}</time>
