e69ada43bdfad3bb46680e9743a582f4ad898308
John Doe <mmd.ayin.2020@gmail.com>
2024-01-04 00:19:50 +0330

    feat(Map): add geolocation custom hook | get user geolocation and fly animition to go to each position

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -2,28 +2,43 @@ import { useNavigate, useSearchParams } from 'react-router-dom'
 import styles from './Map.module.css'
 import { MapContainer, Marker, Popup, TileLayer, useMap, useMapEvents } from 'react-leaflet';
 import { useEffect, useState } from 'react';
-
 import { useCities } from '../contexts/CitiesContext';
 import { PropTypes } from 'prop-types';
+import { useGeolocation } from '../hooks/useGeolocation';
+import Button from './Button';
+
 
 function Map() {
+    const { cities } = useCities();
     const [searchParams] = useSearchParams();
+    const [mapPosition, setMapPosition] = useState([4, 0]);
+    const {
+        isLoading: isLoadingPosition,
+        position: geolocationPosition,
+        getPosition: getGeolocationPosition
+    } = useGeolocation();
     const lat = searchParams.get('lat');
     const lng = searchParams.get('lng');
-
-    const [mapPosition, setMapPosition] = useState([4, 0]);
-    const { cities } = useCities();
 
     useEffect(() => {
         if (!lat || !lng) return;
         const currentPosition = [lat, lng];
 
         setMapPosition(currentPosition);
-    }, [lat, lng, setMapPosition])
+    }, [lat, lng, setMapPosition]);
+
+
+    useEffect(() => {
+        if (!geolocationPosition) return;
+        setMapPosition([geolocationPosition.lat, geolocationPosition.lng]);
+    }, [geolocationPosition]);
 
     return (
         <div className={styles.mapContainer} >
-            <MapContainer center={mapPosition} zoom={6} scrollWheelZoom={true} className={styles.mapContainer}>
+            {!geolocationPosition && <Button type='position' onClick={getGeolocationPosition}>
+                {isLoadingPosition ? 'Loading ...' : "Use your position"}
+            </Button>}
+            <MapContainer center={mapPosition} zoom={13} scrollWheelZoom={true} className={styles.mapContainer}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png"
@@ -45,7 +60,8 @@ function Map() {
 
 function ChangeCenter({ position }) {
     const map = useMap();
-    map.setView(position);
+    map.flyTo(position, map.getZoom())
+
     return null;
 }
 
