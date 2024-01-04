a9802164d2e31824b51319474d70123f4414228c
John Doe <mmd.ayin.2020@gmail.com>
2024-01-04 21:48:21 +0330

    feat(App): add protected route

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import CountryList from "./components/countryList";
 import Form from "./components/Form";
 import { CitiesProvider } from './contexts/CitiesContext'
 import { AuthProvider } from './contexts/FakeAuthContext'
+import ProtectedRoute from './pages/ProtectedRoute';
 
 function App() {
   return (
@@ -21,7 +22,9 @@ function App() {
             <Route index element={<HomePage />} />
             <Route path="product" element={<Product />} />
             <Route path="pricing" element={<Pricing />} />
-            <Route path="app" element={<AppLayout />} >
+            <Route path="app" element={
+              <ProtectedRoute> <AppLayout /> </ProtectedRoute>
+            }>
               <Route index element={<Navigate to='cities' replace />} />
               <Route path="cities" element={<CityList />} />
               <Route path="cities/:id" element={<City />} />
